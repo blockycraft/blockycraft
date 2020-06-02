204736e8a29fe70c176a21cbfce96d879dd38d2e
jrbeverly <jrbeverly@live.com>
2020-06-02 18:49:20 -0400

    Convert chunks from 2D plane to 3D plane

---
+++ blockycraft/Assets/Scripts/Geometry/BlockChunk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+public sealed class BlockChunk
+{
+    public const int SIZE = 8;
+
+    private BlockType[,,] blocks;
+
+    public int Width => blocks.GetLength(0);
+    public int Length => blocks.GetLength(1);
+    public int Depth => blocks.GetLength(2);
+    public BlockType[,,] Blocks => blocks;
+
+    public BlockChunk()
+    {
+        blocks = new BlockType[SIZE, SIZE, SIZE];
+    }
+
+    public IEnumerable<Block> ToList()
+    {
+        for (int x = 0; x < Width; x++)
+            for (int y = 0; y < Length; y++)
+                for (int z = 0; z < Depth; z++)
+                    yield return new Block(x, y, z, Blocks[x, y, z]);
+    }
+}
