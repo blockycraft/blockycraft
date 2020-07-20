02900f6694c1c2fe81d4b4880702407b708f7f47
jrbeverly <jrbeverly@live.com>
2020-07-20 17:34:01 -0400

    Remove unused methods

--- blockycraft/Assets/Scripts/Biome/Biome.cs
+++ blockycraft/Assets/Scripts/Biome/Biome.cs
@@ -7,18 +7,9 @@ public sealed class Biome : ScriptableObject
     [Header("Descriptors")]
     public string Name;
 
-    [Header("Properties")]
-    public int GroundHeight;
-    public int Height;
-
     [Header("Generator")]
     public WorldGenerator Generator;
 
     [Header("Composition")]
     public BiomeBlocks[] Blocks;
-
-    public Biome() {
-        GroundHeight = 1;
-        Height = byte.MaxValue;
-    }
 }
--- blockycraft/Assets/Scripts/Biome/BiomeBlocks.cs
+++ blockycraft/Assets/Scripts/Biome/BiomeBlocks.cs
@@ -2,14 +2,4 @@
 public sealed class BiomeBlocks
 {
     public BlockType Type;
-    public int Minimum;
-    public int Maximum;
-    public float Threshold;
-
-    public BiomeBlocks() {
-        Minimum = 1;
-        Maximum = byte.MaxValue;
-        Threshold = 1.0f;
-    }
 }
-
