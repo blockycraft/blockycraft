bf6c44e43b71ecdcfaf6379208ff8560e9184c46
jrbeverly <jrbeverly@live.com>
2020-07-24 18:27:21 -0400

    Flat domain and rename

--- blockycraft/Assets/Scripts/Biomes/FlatWorldGenerator.cs
+++ blockycraft/Assets/Scripts/Biomes/FlatWorldGenerator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace Assets.Scripts.Biomes
 {
@@ -7,12 +8,17 @@ namespace Assets.Scripts.Biomes
     {
         public override BlockChunk Generate(Biome biome, int chunkX, int chunkZ)
         {
+            var air = biome.Blocks.FirstOrDefault(p => !p.Type.isVisible);
             var chunk = new BlockChunk(chunkX, chunkZ);
             var iterator = chunk.GetIterator();
             foreach (var (x, y, z) in iterator)
             {
-                var idx = (x + y + z) % biome.Blocks.Length;
-                chunk.Blocks[x, y, z] = biome.Blocks[idx].Type;
+                if (air != null && y >= iterator.Height - 1 && Random.value < 0.15f) {
+                    chunk.Blocks[x, y, z] = air.Type;
+                } else {
+                    var idx = y % biome.Blocks.Length;
+                    chunk.Blocks[x, y, z] = biome.Blocks[idx].Type;
+                }
             }
             return chunk;
         }
