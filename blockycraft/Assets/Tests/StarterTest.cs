8a68594af1bd672121b9a30b6c1acbd0a6495608
Jonathan Beverly <jrbeverly@live.com>
2020-08-03 19:23:46 -0400

    Stub out the new Engine style for the project (#76)
    Stub out the Engine style for the project that will support three components: Blockycraft, Editor & Tests.

---
+++ blockycraft/Assets/Tests/StarterTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class StarterTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void StarterTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
