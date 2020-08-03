b7383fb89b5ab86b9e043fabc1b8619ae20a0457
jrbeverly <jrbeverly@live.com>
2020-08-03 17:18:42 -0400

    JSON helper for reading from text

---
+++ blockycraft/Assets/Scripts/JsonHelper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Engine
+{
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        [System.Serializable]
+        private sealed class Wrapper<T>
+        {
+            public T[] Items;
+        }
+    }
+}
