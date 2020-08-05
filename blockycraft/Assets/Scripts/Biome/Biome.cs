95ed69097c4be68ed43af8bb2f6ac3c9ec322807
jrbeverly <jrbeverly@live.com>
2020-08-05 18:16:41 -0400

    Support biome transition maps

--- blockycraft/Assets/Scripts/Biome/Biome.cs
+++ blockycraft/Assets/Scripts/Biome/Biome.cs
@@ -5,6 +5,10 @@ namespace Assets.Scripts.Biome
 {
     public abstract class Biome : ScriptableObject
     {
+        [Header("Mapping")]
+        [Tooltip("Biomes that can be transitioned to from this biome.")]
+        public Biome.Biome[] Transitions;
+
         public abstract ChunkBlocks Generate(Vector3Int coordinate);
     }
 }
