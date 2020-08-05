fada8d5b902b7b27939c31fa25bef1ad430d5a4d
jrbeverly <jrbeverly@live.com>
2020-08-05 18:21:43 -0400

    Common properties to base biome concept

--- blockycraft/Assets/Scripts/Biome/Generator/FlatWorldGenerator.cs
+++ blockycraft/Assets/Scripts/Biome/Generator/FlatWorldGenerator.cs
@@ -7,11 +7,7 @@ namespace Assets.Scripts.Biome.Generator
     [CreateAssetMenu(fileName = "Biome", menuName = "Blockycraft/Biomes/Flat")]
     public sealed class FlatWorldGenerator : Biome
     {
-        [Header("Descriptors")]
-        public string Name;
-
         [Header("Composition")]
-        public BlockType Air;
         public BlockType Shelf;
         public BlockType Top;
 
