0ebb6db0960ef4515fef7942d1c2dc81f2153f49
jrbeverly <jrbeverly@live.com>
2020-08-07 23:42:35 -0400

    Add support for block selector ui

---
+++ blockycraft/Assets/Scripts/Behaviours/BlockSelector.cs
@@ -0,0 +1,16 @@
+﻿using Blockycraft;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class BlockSelector : MonoBehaviour
+{
+    public BlockType[] Types;
+    public int Index;
+    public RawImage Image;
+
+    public void Update()
+    {
+        Image.texture = Types[Index].preview;
+    }
+
+}
