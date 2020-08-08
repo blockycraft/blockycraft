8f63c5ea95ca6ef55e0314bcebbf43b3bcbbf4b1
jrbeverly <jrbeverly@live.com>
2020-08-07 23:48:39 -0400

    Block selector

--- blockycraft/Assets/Scripts/Behaviours/BlockSelector.cs
+++ blockycraft/Assets/Scripts/Behaviours/BlockSelector.cs
@@ -8,9 +8,34 @@ public sealed class BlockSelector : MonoBehaviour
     public int Index;
     public RawImage Image;
 
-    public void Update()
+    public BlockType Selected
+    {
+        get { return Types[Index]; }
+    }
+
+    public void Start()
     {
         Image.texture = Types[Index].preview;
+    }
+
+    public void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f) // forward
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0.0f)
+            {
+                Index++;
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") < 0.0f)
+            {
+                Index--;
+            }
+
+            if (Index < 0) { Index = Types.Length - 1; }
+            else if (Index >= Types.Length) { Index = 0; }
+
+            Image.texture = Types[Index].preview;
+        }
     }
 
 }
